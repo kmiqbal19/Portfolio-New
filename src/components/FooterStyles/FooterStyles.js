6e0d2080c1b9c2c290533e2c4dad4312d6e39c33
kmiqbal19 <km.iqbal01@gmail.com>
2022-07-16 00:24:09 +0200

    completed footer with built info

---
+++ src/components/FooterStyles/FooterStyles.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+export const FooterContainer = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 110vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const TextContainerWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10rem;
+`;
+export const LetsWorkTextContainer = styled.div`
+  width: 40%;
+  height: 400px;
+  /* border: 1px solid red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  div {
+    overflow: hidden;
+  }
+
+  p {
+    font-size: 8rem;
+    font-family: "Ubuntu", sans-serif;
+    color: white;
+  }
+`;
+export const SocialIconsContainer = styled.div`
+  width: 50%;
+  height: 400px;
+  /* border: 1px solid red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    color: white;
+    font-size: 1.5rem;
+    margin-right: 1rem;
+    opacity: 0;
+  }
+  div {
+    display: flex;
+    a {
+      svg {
+        color: white;
+        margin: 2rem;
+        width: 50px;
+        height: 50px;
+        cursor: pointer;
+        &:hover {
+          transform: scale(1.2);
+        }
+        &:nth-child(1) {
+          &:hover {
+            color: red;
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    span {
+      opacity: 1;
+    }
+  }
+`;
+export const FooterTextsContainer = styled.div`
+  width: 80%;
+  height: 100px;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: relative;
+  p {
+    color: white;
+    font-size: 1.5rem;
+    text-align: right;
+    span {
+      text-decoration: underline;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+  ul {
+    padding: 2rem 1rem;
+    position: absolute;
+
+    bottom: 4rem;
+    right: -2rem;
+    background-color: rgba(255, 200, 155, 0.2);
+    li {
+      list-style: none;
+      margin: 1rem;
+      color: white;
+      font-size: 1.5rem;
+      text-align: center;
+    }
+  }
+`;
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,12 +1,76 @@
-import React from "react";
-import styled from "styled-components";
-const FooterContainer = styled.div`
-  background-color: black;
-  width: 100vw;
-  height: 110vh;
-`;
+import React, { useState } from "react";
+import {
+  FooterContainer,
+  TextContainerWrapper,
+  LetsWorkTextContainer,
+  SocialIconsContainer,
+  FooterTextsContainer,
+} from "../components/FooterStyles/FooterStyles";
+import { motion } from "framer-motion";
+import { SiGmail, SiMessenger, SiWhatsapp } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
+
 function Footer() {
-  return <FooterContainer>Footer</FooterContainer>;
+  const [show, setShow] = useState(false);
+  return (
+    <FooterContainer>
+      <TextContainerWrapper>
+        <LetsWorkTextContainer>
+          <div>
+            <motion.p
+              initial={{ y: 70, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.2, ease: "easeOut" }}
+            >
+              Let's Work
+            </motion.p>
+          </div>
+          <br />
+          <div>
+            <motion.p
+              initial={{ y: 80, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.2, ease: "easeOut" }}
+              style={{ marginLeft: "20rem" }}
+            >
+              Together
+            </motion.p>
+          </div>
+        </LetsWorkTextContainer>
+        <SocialIconsContainer>
+          <span>Contact Me</span>
+          <div>
+            <a href="https://github.com/kmiqbal19" title="kmiqbal19">
+              <BsGithub />
+            </a>
+            <a href="loc">
+              <SiGmail title="km.iqbal@gmail.com" />
+            </a>
+            <a href="loc">
+              <SiWhatsapp title="+49 176 776-44904" />
+            </a>
+            <a href="loc">
+              <SiMessenger title="km.iqbal@gmail.com" />
+            </a>
+          </div>
+        </SocialIconsContainer>
+      </TextContainerWrapper>
+      <FooterTextsContainer>
+        <ul style={{ display: !show && "none" }}>
+          <li>React</li>
+          <li>Styled Components</li>
+          <li>GSAP</li>
+          <li>Framer Motion</li>
+          <li>Locmotive Scroll</li>
+        </ul>
+        <p>&copy; Portfolio 2022</p>
+        <p>
+          Designed and developed by me. Built{" "}
+          <span onClick={() => setShow(!show)}>info</span>
+        </p>
+      </FooterTextsContainer>
+    </FooterContainer>
+  );
 }
 
 export default Footer;
