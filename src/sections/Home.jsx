e9028828f6533024261b7eb6a6a7fe9d5fc2f039
kmiqbal19 <km.iqbal01@gmail.com>
2022-07-15 20:52:08 +0200

    added a animated span after selected projects title

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 import CanvasHeroSection from "../components/CanvasHeroSection/CanvasHeroSection.jsx";
 const HomeContainer = styled.div`
   width: (100vw);
@@ -16,12 +17,61 @@ const FixGrayLine = styled.div`
   position: absolute;
   bottom: -0.5rem;
 `;
+const SelectedProjectsTitle = styled.div`
+  width: 400px;
+  /* border: 1px solid red; */
+  position: absolute;
+  bottom: 2%;
+  left: 5%;
+  div {
+    overflow: hidden;
+  }
+
+  p {
+    font-size: 8rem;
+    color: white;
+  }
+`;
+const AnimatedLine = styled(motion.span)`
+  height: 2px;
+  background-color: rgba(255, 255, 255, 0.6);
+  position: absolute;
+  right: 2%;
+  bottom: 10%;
+`;
 // COMPONENT
 function Home() {
   return (
     <HomeContainer>
       <Navbar />
       <CanvasHeroSection />
+      <SelectedProjectsTitle>
+        <div>
+          <motion.p
+            initial={{ y: 70 }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            Selected
+          </motion.p>
+        </div>
+        <br />
+        <div>
+          <motion.p
+            initial={{ y: 80 }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            style={{ marginLeft: "10rem" }}
+          >
+            Projects
+          </motion.p>
+        </div>
+      </SelectedProjectsTitle>
+      <AnimatedLine
+        initial={{ width: "0%" }}
+        whileInView={{ width: "60%" }}
+        transition={{ duration: 2, ease: "backInOut" }}
+      />
       <FixGrayLine />
     </HomeContainer>
   );
