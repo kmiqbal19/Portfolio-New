f6dda62b484bcab5ad2dd1f6f8c15137cebd93ee
kmiqbal19 <km.iqbal01@gmail.com>
2022-07-16 22:27:15 +0200

    finished making about section responsive

---
+++ src/components/AboutStyles/AboutStyles.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+export const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+
+  position: relative;
+
+  background-color: red;
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: -1%;
+  right: 5%;
+  width: 38vw;
+  height: 105vh;
+  border-top: 12vw solid black;
+  border-bottom: 12vw solid black;
+  border-left: 7rem solid black;
+  border-right: 7rem solid black;
+  z-index: 5;
+  @media screen and (max-width: 1300px) {
+    border-left: 4rem solid black;
+    border-right: 4rem solid black;
+    right: 2%;
+    width: 40vw;
+  }
+  @media screen and (max-width: 850px) {
+    width: 80vw;
+    right: 50%;
+    transform: translateX(50%);
+    border-left: 2rem solid black;
+    border-right: 2rem solid black;
+  }
+  @media screen and (max-width: 450px) {
+    width: 90vw;
+  }
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0%;
+  right: 5%;
+  width: 38vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 1300px) {
+    right: 2%;
+    width: 40vw;
+  }
+  @media screen and (max-width: 850px) {
+    width: 80vw;
+    right: 50%;
+    transform: translateX(50%);
+  }
+  @media screen and (max-width: 450px) {
+    width: 90vw;
+  }
+`;
+export const ImageContainer = styled.img`
+  width: 37vw;
+  height: ${window.innerHeight}px;
+  object-fit: cover;
+  @media screen and (max-width: 850px) {
+    width: 80vw;
+  }
+  @media screen and (max-width: 450px) {
+    width: 90vw;
+  }
+`;
+
+export const AboutTextContainer = styled.div`
+  width: 50vw;
+  height: 90vh;
+  /* border: 1px solid white; */
+  margin-right: 10rem;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+  padding: 0rem 4rem;
+  z-index: 2;
+  @media screen and (max-width: 850px) {
+    width: 80vw;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+  @media screen and (max-width: 450px) {
+    width: 90vw;
+    padding: 3rem;
+  }
+  h1 {
+    font-size: calc(4rem + 1.5vw);
+    text-align: left;
+    margin-bottom: 5rem;
+  }
+  p {
+    font-size: calc(1rem + 1vw);
+    line-height: 1.5;
+    /* @media screen and (max-width: 450px) {
+      text-align: center;
+    } */
+  }
+`;
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,5 +1,12 @@
 import React, { useLayoutEffect, useRef } from "react";
-import styled from "styled-components";
+import {
+  Section,
+  Container,
+  ImageContainer,
+  AboutTextContainer,
+  Overlay,
+} from "../components/AboutStyles/AboutStyles";
+import { motion } from "framer-motion";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -7,115 +14,6 @@ import img1 from "../assets/Images/1.webp";
 import img2 from "../assets/Images/2.webp";
 import img3 from "../assets/Images/3.webp";
 
-const Section = styled.section`
-  min-height: 100vh;
-  width: 100vw;
-  margin: 0 auto;
-
-  position: relative;
-
-  background-color: red;
-`;
-
-const Overlay = styled.div`
-  position: absolute;
-  top: -1%;
-  right: 5%;
-  width: 38vw;
-  height: 105vh;
-  border-top: 12vw solid black;
-  border-bottom: 12vw solid black;
-  border-left: 7rem solid black;
-  border-right: 7rem solid black;
-  z-index: 5;
-  @media screen and (max-width: 1300px) {
-    border-left: 4rem solid black;
-    border-right: 4rem solid black;
-    right: 2%;
-    width: 40vw;
-  }
-  @media screen and (max-width: 850px) {
-    width: 80vw;
-    right: 50%;
-    transform: translateX(50%);
-    border-left: 2rem solid black;
-    border-right: 2rem solid black;
-  }
-  @media screen and (max-width: 450px) {
-    width: 90vw;
-  }
-`;
-
-const Container = styled.div`
-  position: absolute;
-  top: 0%;
-  right: 5%;
-  width: 38vw;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  @media screen and (max-width: 1300px) {
-    right: 2%;
-    width: 40vw;
-  }
-  @media screen and (max-width: 850px) {
-    width: 80vw;
-    right: 50%;
-    transform: translateX(50%);
-  }
-  @media screen and (max-width: 450px) {
-    width: 90vw;
-  }
-`;
-const ImageContainer = styled.img`
-  width: 37vw;
-  height: ${window.innerHeight}px;
-  object-fit: cover;
-  @media screen and (max-width: 850px) {
-    width: 80vw;
-  }
-  @media screen and (max-width: 450px) {
-    width: 90vw;
-  }
-`;
-
-const AboutTextContainer = styled.div`
-  width: 50vw;
-  height: 70vh;
-  /* border: 1px solid white; */
-  margin-right: 10rem;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  left: 5%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 4rem;
-  z-index: 2;
-  @media screen and (max-width: 850px) {
-    width: 80vw;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    color: white;
-    background-color: rgba(0, 0, 0, 0.3);
-  }
-  @media screen and (max-width: 450px) {
-    width: 90vw;
-    padding: 3rem;
-  }
-  p {
-    font-size: 2rem;
-    line-height: 1.5;
-    @media screen and (max-width: 450px) {
-      text-align: center;
-    }
-  }
-`;
 const About = () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -180,20 +78,34 @@ const About = () => {
   return (
     <Section ref={ref} id="about">
       <AboutTextContainer>
-        <h1>About Me</h1>
-        <p>
+        <motion.h1
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          About Me
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeOut", delay: 0.4 }}
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
-        </p>
+        </motion.p>
         <br />
-        <p>
+        <motion.p
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut", delay: 0.7 }}
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
-        </p>
+        </motion.p>
       </AboutTextContainer>
 
       <Overlay />
