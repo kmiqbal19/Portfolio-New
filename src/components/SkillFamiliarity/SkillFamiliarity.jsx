ca41509279c4306bc7e0ee17e58cccf24dc94bdf
kmiqbal19 <km.iqbal01@gmail.com>
2023-08-27 20:10:55 +0200

    fix skills familiar

--- src/components/SkillFamiliarity/SkillFamiliarity.jsx
+++ src/components/SkillFamiliarity/SkillFamiliarity.jsx
@@ -1,73 +1,80 @@
 import React from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
 import { skillsFamiliarData } from "./skillsFamiliarData";
 const Container = styled.div`
   width: 100%;
-  height: 30vh;
+  height: 50vh;
   background-color: black;
   display: flex;
   gap: 2;
   flex-direction: row;
-  /* align-items: flex-start; */
-  border: 1px solid red;
-  padding: 5rem 0rem 0rem 5rem;
+ padding-left: 5rem;
+ 
   position: relative;
-  p {
-    /* font-size: calc(1.5rem + 1vw);
-    font-family: "Ubuntu", sans-serif;
-    color: rgba(255, 255, 255, 0.7);
-    width: 60%;
-    margin-left: calc(5rem + 3vw);
-    line-height: 1.5; */
-    @media screen and (max-width: 750px) {
-      width: 80%;
+  @media screen and (max-width: 1440px) {
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+     
     }
-    @media screen and (max-width: 500px) {
-      margin-left: calc(3rem + 3vw);
-    }
-  }
   &::before {
     content: "";
     position: absolute;
-    width: 100%;
+    width: calc(100% + 5rem);
     height: 1rem;
     top: -5px;
+    left: 0;
     background-color: black;
-  }
-`;
-const SkillImage = styled.img`
-  width: 150px;
-  height: 100px;
-  object-fit: contain;
-  margin-right: 2rem;
-  transition: all 0.5s ease;
-  &:hover {
-    transform: scale(1.1);
   }
 `;
 const SkillImageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid green;
   justify-content: center;
-  align-items:center;
+  align-items: center;
+ 
 `;
-const ImageName = styled.p`
-  font-size: 1rem ;
-  text-align: center;
+const SkillImage = styled.img`
+  width: 150px;
+  height: 100px;
+  object-fit: contain;
+  margin-right: 2.5rem;
+  transition: all 0.5s ease;
+  @media screen and (max-width: 640px) {
+    width: 70px;
+  height: 70px;
+     
+    }
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+const ImageName = styled.span`
+  font-size: 1.2rem;
+  margin-top: 1rem;
   color: rgba(255, 255, 255, 0.7);
+  visibility: ${(props) => (props.$hover ? "visible" : "hidden")};
 `;
+const SingleSkill = ({ skill }) => {
+  const [hover, setHover] = React.useState(false);
+  return (
+    <SkillImageContainer>
+      <SkillImage
+        src={skill.img}
+        alt={skill.alt}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      />
+      <ImageName $hover={hover}>{skill.skillName}</ImageName>
+    </SkillImageContainer>
+  );
+};
 const FamiliarSkills = () => {
   return skillsFamiliarData.map((skill) => {
-    return (
-      <SkillImageContainer>
-        <SkillImage src={skill.img} alt={skill.alt} />;
-        <ImageName>{skill.skillName}</ImageName>
-      </SkillImageContainer>
-    );
+    return <SingleSkill skill={skill} />;
   });
 };
+
 function SkillFamiliarity() {
   return (
     <Container>
